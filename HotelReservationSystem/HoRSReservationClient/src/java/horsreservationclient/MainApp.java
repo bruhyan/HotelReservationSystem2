afbb7d83c08acad717f2334bc8eeee190b21d8f0
bruhyan <39240792+bruhyan@users.noreply.github.com>
2018-11-07 23:48:34 +0800

    Update HotelReservationSystem/HoRSReservationClient/src/java/horsreservationclient/MainApp.java

--- HotelReservationSystem/HoRSReservationClient/src/java/horsreservationclient/MainApp.java
+++ HotelReservationSystem/HoRSReservationClient/src/java/horsreservationclient/MainApp.java
@@ -34,39 +34,57 @@ public class MainApp {
         int input = 0;
         while(true) {
             System.out.println("==== Welcome to the HoRS Reservation Client ====");
-            if(loggedInUser == null) {
-                System.out.println("1: Guest Login");
-            }else {
-                System.out.println("1: Do your shit");
-            }
-            
-            System.out.println("2: Guest Registration");
-            System.out.println("3: Exit");
+            System.out.println("1: Guest Login");
+            System.out.println("2: Register as Guest");
+            System.out.println("3: Search Hotel Room");
+            System.out.println("4: Exit");
             if(loggedInUser != null) {
-                System.out.println("4: Guest Logout");
+                System.out.println("5: View My Reservation Details");
+                System.out.println("6: View All My Reservations");
+                System.out.println("7: Logout");
             }
+            System.out.println("===============================================");
             input = 0;
-            while(input < 1 || input > 5) {
+            while(input < 1 || input > 7) {
                 System.out.print(">");
                 input = sc.nextInt();
                 if(input == 1) {
                     if(loggedInUser == null) {
                         doLogin(sc);
                     }else {
-                        doGuestOperations(sc);
+                        System.out.println("Current logged in user: "+loggedInUser.getEmail());
                     }
                 }else if(input == 2) {
                     doGuestRegistration(sc);
                 }else if(input == 3) {
+                    doSearchHotelRoom(sc);
+                }else if(input == 4) {
                     break;
-                }else if(input == 4 && loggedInUser != null) {
+                }else if(input == 5 && loggedInUser != null) {
+                    doViewMyReservationDetails(sc);
+                }else if(input == 6 && loggedInUser != null) {
+                    doViewAllMyReservations(sc);
+                }else if(input == 7 && loggedInUser != null) {
                     doLogout();
                 }
             }
-            if(input == 3) {
+            if(input == 4) {
                 break;
             }
         }
+    }
+    
+    public void doSearchHotelRoom(Scanner sc) {
+        System.out.println("TBC");
+        
+    }
+    
+    public void doViewMyReservationDetails(Scanner sc) {
+        System.out.println("TBC");
+    }
+    
+    public void doViewAllMyReservations(Scanner sc) {
+        System.out.println("TBC");
     }
     
     public void doLogin(Scanner sc) {
@@ -109,10 +127,6 @@ public class MainApp {
         CustomerEntity customer = new CustomerEntity(email, contactNumber, firstName, lastName, password);
         customer = customerControllerRemote.createCustomerEntity(customer);
         System.out.println("Customer "+customer.getEmail()+" successfully created");
-    }
-    
-    public void doGuestOperations(Scanner sc) {
-        
     }
     
     public void doLogout() {
