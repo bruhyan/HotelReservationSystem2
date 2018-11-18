9fdacd2d41def92db192a14c34e465a1bcebecf1
bruhyan <39240792+bruhyan@users.noreply.github.com>
2018-11-18 17:06:40 +0800

    clean up exceptions and enums

--- HotelReservationSystem/HoRSLibrary/src/util/exception/NoReservationFoundException.java
+++ HotelReservationSystem/HoRSLibrary/src/util/exception/NoReservationFoundException.java
@@ -1,29 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package util.exception;
 
-/**
- *
- * @author Bryan
- */
+
 public class NoReservationFoundException extends Exception {
 
-    /**
-     * Creates a new instance of <code>NoReservationFoundException</code>
-     * without detail message.
-     */
+    
     public NoReservationFoundException() {
     }
 
-    /**
-     * Constructs an instance of <code>NoReservationFoundException</code> with
-     * the specified detail message.
-     *
-     * @param msg the detail message.
-     */
     public NoReservationFoundException(String msg) {
         super(msg);
     }
